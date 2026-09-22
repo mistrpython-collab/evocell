6c51cdecfb500bd81d375a5d7efbe5a251a74235
mistrpython-collab <mistrpython@gmail.com>
2026-09-23 06:40:14 +0700

    Initialize Phaser game with configuration

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.70.0/lib/phaser.min.js';
+import config from './config.js';
+
+const game = new Phaser.Game(config);
+
+export default game;
