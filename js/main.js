e73c09ee0089590bfcb2fccc2b772fe653d7bab4
mistrpython-collab <mistrpython@gmail.com>
2026-09-23 06:51:56 +0700

    Add comments to main.js for better understanding
    Add a comment to the main.js file for clarity.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-import Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3.70.0/lib/phaser.min.js';
+// main.js - Entry point untuk game EvoCell
 import config from './config.js';
 
+// Inisialisasi game Phaser dengan konfigurasi
 const game = new Phaser.Game(config);
 
 export default game;
