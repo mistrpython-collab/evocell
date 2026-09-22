e17241747006fe3665aef85c54b42da8749382bd
mistrpython-collab <mistrpython@gmail.com>
2026-09-23 06:46:00 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,31 @@
+import BootScene from './scenes/boot-scene.js';
+import MenuScene from './scenes/menu-scene.js';
+import RoleSelectScene from './scenes/role-select-scene.js';
+import ModeSelectScene from './scenes/mode-select-scene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    backgroundColor: '#000000',
+    parent: 'game-container',
+    scene: [
+        BootScene,
+        MenuScene,
+        RoleSelectScene,
+        ModeSelectScene
+    ],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
+};
+
+export default config;
