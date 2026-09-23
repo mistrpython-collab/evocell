eb9d15d0ed15c744f8d3e9350325370e4216309d
mistrpython-collab <mistrpython@gmail.com>
2026-09-23 07:25:16 +0000

    perubahan lagi2

--- js/scenes/intro-scene.js
+++ js/scenes/intro-scene.js
@@ -1,34 +1,32 @@
-// intro-scene.js - Intro scene dengan Earth + EvoCell logo (6 detik)
+// intro-scene.js - Intro scene sederhana dan reliable
 export default class IntroScene extends Phaser.Scene {
     constructor() {
         super('IntroScene');
+        console.log('IntroScene: constructor called');
     }
 
     create() {
+        console.log('IntroScene: create started');
+        
         const width = this.cameras.main.width;
         const height = this.cameras.main.height;
         
         // Background hitam
         this.add.rectangle(width/2, height/2, width, height, 0x000000);
+        console.log('IntroScene: background added');
         
-        // Earth di tengah (background logo)
+        // Earth di tengah
         const earth = this.add.image(width/2, height/2, 'earth');
         earth.setScale(0.7);
+        console.log('IntroScene: earth added');
         
-        // Animasi Earth (zoom in perlahan)
-        this.tweens.add({
-            targets: earth,
-            scale: 0.95,
-            duration: 6000,
-            ease: 'Sine.easeInOut'
-        });
-        
-        // Logo EvoCell di atas Earth
+        // Logo EvoCell
         const logo = this.add.image(width/2, height/2, 'logo');
         logo.setScale(0.85);
-        
-        // Animasi logo (fade in)
         logo.setAlpha(0);
+        console.log('IntroScene: logo added');
+        
+        // Animasi logo fade in
         this.tweens.add({
             targets: logo,
             alpha: 1,
@@ -36,14 +34,20 @@ export default class IntroScene extends Phaser.Scene {
             ease: 'Power2'
         });
         
-        // Setelah 6 detik, pindah ke Menu Scene
+        // Animasi Earth zoom in
+        this.tweens.add({
+            targets: earth,
+            scale: 0.95,
+            duration: 6000,
+            ease: 'Sine.easeInOut'
+        });
+        
+        console.log('IntroScene: animations started, waiting 6 seconds...');
+        
+        // Setelah 6 detik, LANGSUNG pindah ke MenuScene (TANPA fadeOut)
         this.time.delayedCall(6000, () => {
-            // Fade out effect
-            this.cameras.main.fadeOut(800, 0, 0, 0);
-            
-            this.cameras.main.once('camera-fade-out-complete', () => {
-                this.scene.start('MenuScene');
-            });
+            console.log('IntroScene: timeout reached, starting MenuScene NOW!');
+            this.scene.start('MenuScene');
         });
     }
 }
